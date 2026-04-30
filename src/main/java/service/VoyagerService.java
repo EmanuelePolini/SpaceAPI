31815589c495a5104631621a72cffdf433c4f868
EmanuelePolini <emanuelepolini07@gmail.com>
2026-04-30 11:51:19 +0200

    Add method to retrieve Voyager data

--- src/main/java/service/VoyagerService.java
+++ src/main/java/service/VoyagerService.java
@@ -1,5 +1,15 @@
 package service;
+import model.VoyagerData;
 
 public class VoyagerService {
-
+	public VoyagerData getVoyagerData() {
+		VoyagerData data = new VoyagerData();
+		
+		data.setName("Voyager 1");
+		data.setDistanzaUA(160.5);
+		data.setVelocitaKmS(10220);
+		data.setYearsInSpace(55);
+		
+		return data;
+	}
 }
