69317ab5b643a343f1a38a30368dfd998a934f8b
EmanuelePolini <emanuelepolini07@gmail.com>
2026-04-30 11:51:47 +0200

    Add GET endpoint for Voyager status

--- src/main/java/resource/VoyagerResource.java
+++ src/main/java/resource/VoyagerResource.java
@@ -5,8 +5,21 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import model.VoyagerData;
+import service.VoyagerService;
+
+//questa è la classe dove JAX-RS gestisce tutto ciò che parte da /voyager
 @Path("/voyager")
 public class VoyagerResource {
-	
+	//richiesta lettura
+	@GET
+	//sottopath
+	@Path("/status")
+	@Produces(MediaType.APPLICATION_JSON)
+	public VoyagerData getStatus() {
+		VoyagerService service = new VoyagerService();
+		//ritorna un oggetto e JAX-RS lo trasforma in JSON
+		return service.getVoyagerData();
+	}
 	
 }
