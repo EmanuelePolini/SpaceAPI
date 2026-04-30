2b6613095dce1da56aa6232812ce2623ee32b9d8
EmanuelePolini <emanuelepolini07@gmail.com>
2026-04-30 11:50:52 +0200

    Implement VoyagerData class with attributes and methods
    Added fields and methods for Voyager data attributes.

--- src/main/java/model/VoyagerData.java
+++ src/main/java/model/VoyagerData.java
@@ -1,5 +1,42 @@
 package model;
+//questa classe si occupa di contenere i dati necessari alle richieste
+//contiene nome, anni nello spazio,velocitòà, distanza
 
 public class VoyagerData {
-
+	private String name;
+	private int yearsInSpace;
+	private double velocitaKmS;
+	private double distanzaUA;
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setYearsInSpace(int yearsInSpace) {
+		this.yearsInSpace = yearsInSpace;
+	}
+	
+	public void setVelocitaKmS(double velocitaKmS) {
+		this.velocitaKmS = velocitaKmS;
+	}
+	
+	public void setDistanzaUA(double distanzaUA) {
+		this.distanzaUA = distanzaUA;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getYears() {
+		return yearsInSpace;
+	}
+	
+	public double getVelocitaKmS() {
+		return velocitaKmS;
+	}
+	
+	public double getDistanzaUA() {
+		return distanzaUA;
+	}
 }
