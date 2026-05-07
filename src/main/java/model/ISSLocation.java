5ba8f5052d14712b0daad9a83373e6e5e7b2738f
EmanuelePolini <emanuelepolini07@gmail.com>
2026-05-07 12:23:26 +0200

    Implement getters and setters for ISSLocation
    Added methods to set and get latitude and longitude.

--- src/main/java/model/ISSLocation.java
+++ src/main/java/model/ISSLocation.java
@@ -1,5 +1,27 @@
 package model;
 
-public class ISSLocation {
+//questa classe conterrà i dati per monitorare la posizione della stazione spaziale internazionale in tempo reale
+//l'API di riferimento con i dati aggiornati è questa: http://api.open-notify.org/iss-now.json
 
+public class ISSLocation {
+	private String latitude;
+	private String longitude;
+	
+	public void setLatitude(String latitude) {
+		this.latitude = latitude;
+	}
+	
+	public void setLongitude(String longitude) {
+		this.longitude = longitude;
+	}
+
+	public String getLatitude() {
+		return latitude;
+	}
+	
+	public String getLongitude() {
+		return longitude;
+	}
+	
+	
 }
