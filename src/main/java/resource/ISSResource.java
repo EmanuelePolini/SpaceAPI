bf987a929eec8278530c47eb4e27d94ee9b4b8db
EmanuelePolini <emanuelepolini07@gmail.com>
2026-05-07 12:23:08 +0200

    Implement GET endpoint for ISS location
    Added REST endpoint to retrieve ISS location.

--- src/main/java/resource/ISSResource.java
+++ src/main/java/resource/ISSResource.java
@@ -1,5 +1,21 @@
 package resource;
 
-public class ISSResource {
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
+import model.ISSLocation;
+import service.ISSService;
+
+@Path("/iss")
+public class ISSResource {
+	@GET
+	@Path("/location")
+	@Produces(MediaType.APPLICATION_JSON)
+	public ISSLocation getLocation() {
+		ISSService service = new ISSService();
+		
+		return service.getISSLocation();
+	}
 }
