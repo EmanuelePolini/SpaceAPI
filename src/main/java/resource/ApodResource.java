73f29920b89f110356a786b727aa633b6a871fcb
EmanuelePolini <emanuelepolini07@gmail.com>
2026-05-08 10:38:43 +0200

    Add REST endpoint to fetch APOD data

--- src/main/java/resource/ApodResource.java
+++ src/main/java/resource/ApodResource.java
@@ -1,5 +1,21 @@
 package resource;
 
-public class ApodResource {
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
+import model.ApodData;
+import service.ApodService;
+
+@Path("/apod")
+public class ApodResource {
+	@GET
+	@Path("/data")
+	@Produces(MediaType.APPLICATION_JSON)
+	public ApodData getData() {
+		ApodService apod = new ApodService();
+		
+		return apod.getApodData();
+	}
 }
