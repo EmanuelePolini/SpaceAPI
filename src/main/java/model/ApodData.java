28ff760860f9322b70373b2edd12cde1d00a68e0
EmanuelePolini <emanuelepolini07@gmail.com>
2026-05-08 10:39:05 +0200

    Add fields and methods to ApodData class
    Added fields for date, explanation, url, and title with corresponding getters and setters.

--- src/main/java/model/ApodData.java
+++ src/main/java/model/ApodData.java
@@ -1,5 +1,41 @@
 package model;
 
 public class ApodData {
-
+	private String date;
+	private String explanation;
+	private String url;
+	private String title;
+	
+	
+	public void setDate(String date) {
+		this.date = date;
+	}
+	
+	public String getDate() {
+		return this.date;
+	}
+	
+	public void setExplanation(String explanation) {
+		this.explanation = explanation;
+	}
+	
+	public String getExplanation() {
+		return this.explanation;
+	}
+	
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+	public String getUrl() {
+		return this.url;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public String getTitle() {
+		return this.title;
+	}
 }
