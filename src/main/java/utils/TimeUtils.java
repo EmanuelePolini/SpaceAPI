e5c9578b0b820bcc4cbeb35cfdf430086afef004
EmanuelePolini <emanuelepolini07@gmail.com>
2026-05-09 20:05:19 +0200

    Add methods to calculate years and days since a date

--- src/main/java/utils/TimeUtils.java
+++ src/main/java/utils/TimeUtils.java
@@ -1,5 +1,17 @@
 package utils;
 
-public class TimeUtils {
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
+public class TimeUtils {
+	
+	//calcolo degli anni trascorsi da una determinata data 
+	public static long calculateYearsSinceLaunch(LocalDate startDate) {
+		return ChronoUnit.YEARS.between(startDate, LocalDate.now());
+	}
+	
+	//calcolo dei secondi trascorsi da una determinata data (per applicarlo alla formula della distanza in km)
+	public static long calculateDaysSinceLaunch(LocalDate startDate) {
+		return ChronoUnit.DAYS.between(startDate, LocalDate.now());
+	}
 }
