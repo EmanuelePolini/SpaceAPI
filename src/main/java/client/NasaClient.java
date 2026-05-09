6fc26e49d9556df6cc089c010950f0c17b0d6cde
EmanuelePolini <emanuelepolini07@gmail.com>
2026-05-09 20:05:59 +0200

    Implement OpenNotifyClient to fetch ISS location data
    Added OpenNotifyClient class to call the ISS API and retrieve data.

--- src/main/java/client/NasaClient.java
+++ src/main/java/client/NasaClient.java
@@ -1,5 +1,34 @@
 package client;
 
-public class NasaClient {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
+public class OpenNotifyClient {
+	public String callISSApi() {
+		String urlString = "http://api.open-notify.org/iss-now.json";
+		String jsondata = "";
+		
+		try {
+			URL url = new URL(urlString);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+			
+			BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			String output="";
+			
+			while((output = reader.readLine()) != null) {
+				jsondata += output;
+			}
+			
+			reader.close();
+			
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return jsondata;
+	}
 }
