017d2484f1a8a7efe4980c34412fb02aeb93a542
EmanuelePolini <emanuelepolini07@gmail.com>
2026-05-09 20:06:13 +0200

    Implement NasaClient to fetch data from NASA API
    Added NasaClient class to call NASA API for data.

--- src/main/java/client/OpenNotifyClient.java
+++ src/main/java/client/OpenNotifyClient.java
@@ -1,5 +1,35 @@
 package client;
 
-public class OpenNotifyClient {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
+public class NasaClient {
+	public String callApi() {
+		String urlString = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+		String jsondata = "";
+		
+		try {
+			
+			@SuppressWarnings("deprecation")
+			URL url = new URL(urlString);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+			
+			BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			String output="";
+			
+			while((output = reader.readLine()) != null) {
+				jsondata += output;
+			}
+			
+			reader.close();
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return jsondata;
+	}
 }
